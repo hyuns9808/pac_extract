84095839e4043b51be09897776c11ed8fa67ea8a
hyuns9808 <78573198+hyuns9808@users.noreply.github.com>
2025-08-07 18:05:16 +0900

    Completed Checkov KICS parser

--- src/fetch_pac/tool_parsers/get_checkov.py
+++ src/fetch_pac/tool_parsers/get_checkov.py
@@ -2,8 +2,10 @@
 Functions related to getting relevant Checkov PaCs
 '''
 import pandas as pd
-import re
 
+# Change file path of md if necessary
+file_path = "data\\Checkov\\5.Policy Index\\all.md"
+# Correctly parses code into provider name
 id_to_provider = {
     "ADO": "Azure DevOps",
     "ALI": "Alibaba Cloud",
@@ -23,18 +25,28 @@ id_to_provider = {
     "GIT": "GitHub",
     "GITHUB": "GitHub Configuration",
     "GITLAB": "GitLab",
-    "GITLABCI": 
+    "GITLABCI": "GitLab CI",
+    "GLB": "GitLab Branch",
+    "IBM": "IBM Cloud",
+    "K8S": "Kubernetes",
+    "LIN": "Linode",
+    "NCP": "Naver Cloud",
+    "OCI": "Oracle Cloud Infrastructure",
+    "OPENAPI": "OpenAPI",
+    "OPENSTACK": "OpenStack",
+    "PAN": "PAN-OS",
+    "SECRET": "Secrets",
+    "TC": "Tencent Cloud",
+    "TF": "Terraform",
+    "YC": "Yandex Cloud"
 }
-
-
-# May fix to use more service-specific md files
-def get_checkov_pac():
-    df = load_md_table("data\Checkov\\5.Policy Index\\all.md")
-    return df
     
-def load_md_table(filepath):
+def get_checkov_pac(file_path=file_path):
+    '''
+    Creates final pandas df for Checkov
+    '''
     # Read markdown table as raw text and manually parse
-    with open(filepath, 'r', encoding='utf-8') as f:
+    with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
     # Strip lines and ignore separators
     lines = [line.strip() for line in lines if line.strip() and line.startswith('|')]
@@ -45,12 +57,24 @@ def load_md_table(filepath):
     for line in lines[2:]:
         row = [cell.strip() for cell in line.split('|')[1:-1]]
         data.append(row)
-    result = pd.DataFrame(data, columns=headers)
+    df = pd.DataFrame(data, columns=headers)
     # Patch DF to common format
-    result['Tool'] = 'Checkov'
+    # Tool-ID-Name-IaC-Provider-Severity-Query Document-Related Document
+    result = pd.DataFrame()
+    result["Tool"] = ["Checkov"] * len(df)
+    result["ID"] = df["Id"]
+    result["IaC"] = df["IaC"]
+    name_ptn = r"([^_]+)_([^_]+)_([^_]+)"
+    result["Provider"] = df["Id"].str.extract(name_ptn)[1].map(id_to_provider)
+    result["Severity"] = "NaN"
+    result["Query Document"] = df["Resource Link"]
+    result["Related Document"] = "NaN"
     return result
 
 def filter_policies(df, partial_id, iac_type, keyword):
+    '''
+    Returns dataframe that consists of partial id, iac type and keyword match
+    '''
     keyword = keyword.lower()
     return df[
         df['Id'].str.contains(partial_id, case=False, na=False) &
@@ -61,11 +85,8 @@ def filter_policies(df, partial_id, iac_type, keyword):
         )
     ]
 
+'''
 if __name__ == '__main__':
     df = get_checkov_pac()
     print(df)
-    name_ptn = r"([^_]+)_([^_]+)_([^_]+)"
-    df_extracted = df["Id"].str.extract(name_ptn)[1]
-    df_unique = df_extracted.drop_duplicates().reset_index(drop=True)
-    print(df_unique)
-    print(df_extracted.dtype)
+'''
--- src/fetch_pac/tool_parsers/get_kics.py
+++ src/fetch_pac/tool_parsers/get_kics.py
@@ -3,20 +3,43 @@ Functions related to getting relevant KICS PaCs
 Reads stored .csv file for all queries, searches relevant ones and returns it as a dataframe
 '''
 import pandas as pd
+
+# Change file path of csv if necessary
+file_path = "data\\KICS\\all_queries.csv"
+# Correctly parses query document link into provider name
+id_to_provider = {
+    "alicloud": "Alibaba Cloud",
+    "aws": "AWS",
+    "azure": "Azure",
+    "nan": "NaN",
+    "nifcloud": "Nifcloud",
+    "gcp": "Google Cloud Platform",
+    "tencentcloud": "Tencent Cloud",
+}
     
-# May fix to use more service-specific md files
-def get_kics_pac():
-    df = load_csv_table("data\\KICS\\all_queries.csv")
-    print(df)
-    
-def load_csv_table(filepath):
+def get_kics_pac(file_path=file_path):
+    '''
+    Wrapper function for creating pd dataframe
+    '''
     # Read CSV as a dataframe
-    result = pd.read_csv(filepath)
+    df = pd.read_csv(file_path)
     # Patch DF to common format
-    result['Tool'] = 'KICS'
+    # Tool-ID-Name-IaC-Provider-Severity-Query Document-Related Document
+    result = pd.DataFrame()
+    result["Tool"] = ["KICS"] * len(df)
+    result["ID"] = df["Query ID"]
+    result["IaC"] = df["Platform "]
+    name_ptn = r"https://docs.kics.io/latest/queries/[^/]+/([^/]+)/[^/]+"
+    result["Provider"] = df["Query Details"].str.extract(name_ptn)[0].map(id_to_provider)
+    result["Severity"] = df["Severity "]
+    result["Query Document"] = df["Documentation"]
+    result["Related Document"] = df["Query Details"]
     return result
 
 def filter_policies(df, partial_id, iac_type, keyword):
+    '''
+    Returns dataframe that consists of partial id, iac type and keyword match
+    '''
     keyword = keyword.lower()
     return df[
         df['Id'].str.contains(partial_id, case=False, na=False) &
@@ -27,5 +50,8 @@ def filter_policies(df, partial_id, iac_type, keyword):
         )
     ]
 
+'''
 if __name__ == '__main__':
-    get_kics_pac()
+    df = get_kics_pac()
+    print(df)
+'''
